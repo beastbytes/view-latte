a4481c41c750783f564ab60ea46984f4297d98e1
Chris <chris@clyates.plus.com>
2025-02-01 16:21:23 +0000

    Add LatteFactoryTest and support

---
+++ tests/LatteFactoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace BeastBytes\View\Latte\Tests;
+
+use BeastBytes\View\Latte\LatteFactory;
+use BeastBytes\View\Latte\Tests\Support\Providers\TestFilter;
+use BeastBytes\View\Latte\Tests\Support\Providers\TestFunction;
+use Latte\Engine;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class LatteFactoryTest extends TestCase
+{
+    private string $cacheDir;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->cacheDir = __DIR__ . '/public/tmp';
+    }
+
+    #[Test]
+    public function create(): void
+    {
+        $factory = new LatteFactory($this->cacheDir);
+        $latte = $factory->create();
+
+        $this->assertInstanceOf(Engine::class, $latte);
+    }
+
+    #[Test]
+    public function createWithProviders(): void
+    {
+        $factory = new LatteFactory(
+            $this->cacheDir,
+            [new TestFilter],
+            [new TestFunction],
+        );
+        $latte = $factory->create();
+
+        $filters = $latte->getFilters();
+        $functions = $latte->getFunctions();
+
+        $this->assertArrayHasKey('testFilter', $filters);
+        $this->assertArrayHasKey('testFunction', $functions);
+    }
+
+    #[Test]
+    public function providers(): void
+    {
+        $factory = new LatteFactory(
+            $this->cacheDir,
+            [new TestFilter],
+            [new TestFunction],
+        );
+        $latte = $factory->create();
+
+        $result = $latte->renderToString(__DIR__ . '/public/views/providers.latte');
+
+        $this->assertStringContainsString('<p>EDCBA</p>', $result);
+        $this->assertStringContainsString('<p>4</p>', $result);
+    }
+}
