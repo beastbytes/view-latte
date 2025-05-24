0f8a95c0dc32106a53a6a238f372ad4d751db8fc
Chris <chris@clyates.plus.com>
2025-05-24 21:30:31 +0100

    Update LinkExtension and tests

--- src/Extensions/Link/LinkExtension.php
+++ src/Extensions/Link/LinkExtension.php
@@ -4,20 +4,68 @@ declare(strict_types=1);
 
 namespace BeastBytes\View\Latte\Extensions\Link;
 
-final class LinkExtension
+use Latte\Compiler\NodeHelpers;
+use Latte\Compiler\Tag;
+use Latte\Compiler\Nodes\Php;
+use Latte\Essential\Nodes\PrintNode;
+use Latte\Extension;
+use Yiisoft\Router\UrlGeneratorInterface;
+
+final class LinkExtension extends Extension
 {
+    public function __construct(private readonly UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    /*
     public function getFunctions(): array
     {
         return [
             'link' => ''
         ];
     }
+*/
 
     public function getTags(): array
     {
         return [
-            'n:href' => Nodes\LinkNode::create(...),
-            'link' => Nodes\LinkNode::create(...),
+            'n:href' => [$this, 'parseLink'],
+            'link' => [$this, 'parseLink'],
         ];
+    }
+
+    public function parseLink(Tag $tag): PrintNode
+    {
+        $tag->outputMode = $tag::OutputKeepIndentation;
+        $tag->expectArguments();
+        $node = new PrintNode;
+        $node->expression = $tag->parser->parseUnquotedStringOrExpression();
+        $args = new Php\Expression\ArrayNode;
+        if ($tag->parser->stream->tryConsume(',')) {
+            $args = $tag->parser->parseArguments();
+        }
+
+        $node->modifier = $tag->parser->parseModifier();
+        $node->modifier->escape = false;
+
+        $expr = self::toValue($node->expression);
+        $values = self::toValue($args);
+
+        if ($tag->isNAttribute()) {
+            $url = ' href="' . $this->urlGenerator->generate($expr, ...$values) . '"';
+        } else {
+            $url = $this->urlGenerator->generateAbsolute($expr, ...$values);
+        }
+
+        $node->expression = new Php\Scalar\StringNode($url);
+        return $node;
+    }
+    public static function toValue($args): mixed
+    {
+        try {
+            return NodeHelpers::toValue($args, constants: true);
+        } catch (\InvalidArgumentException) {
+            return null;
+        }
     }
 }
---
+++ tests/Extensions/LinkExtensionTest.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeastBytes\View\Latte\Tests\Extensions;
+
+use BeastBytes\View\Latte\Extensions\Link\LinkExtension;
+use BeastBytes\View\Latte\Tests\TestCase;
+use Generator;
+use HttpSoft\Message\Uri;
+use PHPUnit\Framework\Attributes\BeforeClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use Yiisoft\Router\CurrentRoute;
+use Yiisoft\Router\FastRoute\UrlGenerator;
+use Yiisoft\Router\Route;
+use Yiisoft\Router\RouteCollection;
+use Yiisoft\Router\RouteCollector;
+use Yiisoft\Router\UrlGeneratorInterface;
+
+final class LinkExtensionTest extends TestCase
+{
+    private const URL = 'https://example.com';
+
+    private static RouteCollection $routeCollection;
+    private static UrlGeneratorInterface $urlGenerator;
+
+    #[BeforeClass]
+    public static function beforeClass(): void
+    {
+        $routes = [
+            Route::get('/test/item')
+                ->name('item-index')
+                ->action(fn() => null)
+            ,
+            Route::get('/test/item/create')
+                ->name('item-create')
+                ->action(fn() => null)
+            ,
+            Route::get('/test/item/update/{id:\d+}')
+                ->name('item-update')
+                ->action(fn() => null)
+            ,
+            Route::get('/test/item/{id:\d+}')
+                ->name('item-view')
+                ->action(fn() => null)
+            ,
+        ];
+
+        self::$routeCollection = new RouteCollection((new RouteCollector())->addRoute(...$routes));
+
+        $currentRoute = new CurrentRoute();
+        $currentRoute->setUri(new Uri(self::URL));;
+
+        self::$urlGenerator = new UrlGenerator(self::$routeCollection, $currentRoute);
+
+        self::$extensions[] = new LinkExtension(self::$urlGenerator);
+        parent::beforeClass();
+    }
+
+    #[Test]
+    #[DataProvider('routeProvider')]
+    public function n_href_attribute(string $name, array $arguments): void
+    {
+        $expected = sprintf(
+            '<a href="%s">Test</a>',
+            self::$urlGenerator->generate($name, $arguments)
+        );
+        $template = '<a n:href="%s, [%s]">Test</a>';
+        $this->assert($expected, $template, $name, $arguments);
+    }
+
+    #[Test]
+    #[DataProvider('routeProvider')]
+    public function link_tag(string $name, array $arguments): void
+    {
+        $expected = self::$urlGenerator->generateAbsolute($name, $arguments);
+        $template = '{link %s, [%s]}';
+        $this->assert($expected, $template, $name, $arguments);
+    }
+
+    public static function routeProvider(): Generator
+    {
+        yield [
+            'name' => 'item-index',
+            'arguments' => [],
+        ];
+        yield [
+            'name' => 'item-create',
+            'arguments' => [],
+        ];
+        yield [
+            'name' => 'item-update',
+            'arguments' => ['id' => '69'],
+        ];
+        yield [
+            'name' => 'item-view',
+            'arguments' => ['id' => '69'],
+        ];
+    }
+
+    private function assert(string $expected, string $template, string $routeName, array $routeArguments): void
+    {
+        $arguments = '';
+        foreach ($routeArguments as $key => $value) {
+            $arguments .= $key . ' => ' . $value . ', ';
+        }
+
+        $template = sprintf(
+            $template,
+            $routeName,
+            rtrim($arguments, ', ')
+        );
+        $templateFile = self::TEMPLATE_DIR . DIRECTORY_SEPARATOR . '_' . md5($template) . '.latte';
+
+        file_put_contents($templateFile, $template);
+
+        $actual = self::$latte->renderToString($templateFile);
+
+        $this->assertSame($expected, $actual);
+    }
+}
